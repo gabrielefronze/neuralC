0e42bfc80ca244aa227f73cea2da4bc86cfffbb8
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2018-03-05 22:32:54 +0100

    Initial commit of perceptron class

---
+++ perceptron.cxx
@@ -0,0 +1,5 @@
+//
+// Created by Gabriele Gaetano Fronzé on 05/03/2018.
+//
+
+#include "perceptron.h"
---
+++ perceptron.h
@@ -0,0 +1,13 @@
+//
+// Created by Gabriele Gaetano Fronzé on 05/03/2018.
+//
+
+#ifndef NEURALC_PERCEPTRON_H
+#define NEURALC_PERCEPTRON_H
+
+class perceptron
+{
+
+};
+
+#endif //NEURALC_PERCEPTRON_H
